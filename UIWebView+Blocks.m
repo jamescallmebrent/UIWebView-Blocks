ffe8f31c93953dbbdd7006ef6c0473198372bdbd
Brent Hargrave <brent@brent.is>
2014-03-15 17:40:52 -0400

    set experimental TRUE_END_REPORT true by default

--- UIWebView+Blocks.m
+++ UIWebView+Blocks.m
@@ -28,7 +28,7 @@ static uint __loadedWebItems;
 +(UIWebView *)loadHTMLString:(NSString *)htmlString
                       loaded:(void (^)(UIWebView *webView))loadedBlock
                       failed:(void (^)(UIWebView *webView, NSError *error))failureBlock{
-    
+
     return [self loadHTMLString:htmlString loaded:loadedBlock failed:failureBlock loadStarted:nil shouldLoad:nil];
 }
 
@@ -42,11 +42,11 @@ static uint __loadedWebItems;
     __failureBlock = failureBlock;
     __loadStartedBlock = loadStartedBlock;
     __shouldLoadBlock = shouldLoadBlock;
-    
+
     UIWebView *webView = [[UIWebView alloc] init];
     webView.delegate = (id)[self class];
     [webView loadHTMLString:htmlString baseURL:nil];
-    
+
     return webView;
 }
 
@@ -56,40 +56,40 @@ static uint __loadedWebItems;
               loadStarted:(void (^)(UIWebView *webView))loadStartedBlock
                shouldLoad:(BOOL (^)(UIWebView *webView, NSURLRequest *request, UIWebViewNavigationType navigationType))shouldLoadBlock{
     __loadedWebItems    = 0;
-    
+
     __loadedBlock       = loadedBlock;
     __failureBlock      = failureBlock;
     __loadStartedBlock  = loadStartedBlock;
     __shouldLoadBlock   = shouldLoadBlock;
-    
+
     UIWebView *webView  = [[UIWebView alloc] init];
     webView.delegate    = (id) [self class];
-    
+
     [webView loadRequest: request];
-    
+
     return webView;
 }
 
 #pragma mark - Private Static delegate
 +(void)webViewDidFinishLoad:(UIWebView *)webView{
     __loadedWebItems--;
-    
+
     if(__loadedBlock && (!TRUE_END_REPORT || __loadedWebItems == 0)){
         __loadedWebItems = 0;
         __loadedBlock(webView);
     }
 }
 
-+(void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error{    
++(void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error{
     __loadedWebItems--;
-    
+
     if(__failureBlock)
         __failureBlock(webView, error);
 }
 
-+(void)webViewDidStartLoad:(UIWebView *)webView{    
++(void)webViewDidStartLoad:(UIWebView *)webView{
     __loadedWebItems++;
-    
+
     if(__loadStartedBlock && (!TRUE_END_REPORT || __loadedWebItems > 0))
         __loadStartedBlock(webView);
 }
@@ -97,7 +97,7 @@ static uint __loadedWebItems;
 +(BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType{
     if(__shouldLoadBlock)
         return __shouldLoadBlock(webView, request, navigationType);
-    
+
     return YES;
 }
 
